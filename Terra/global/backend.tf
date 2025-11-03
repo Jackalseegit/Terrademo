df7de496868554d588e1d7fd2c0b4ace250cbffe
Jackalseegit <117627375+Jackalseegit@users.noreply.github.com>
2025-11-03 22:29:00 +0530

    Delete Terra directory

--- Terra/global/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "helpelp"
-    key            = "terraform/prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
--- Terra/global/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "region1" {
-  value = var.region1
-}
-
-output "region2" {
-  value = var.region2
-}
