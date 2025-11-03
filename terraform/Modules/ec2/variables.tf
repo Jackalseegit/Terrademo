999c5799ba4a98a6734940fcf1e4dffd4675a757
Jackalseegit <117627375+Jackalseegit@users.noreply.github.com>
2025-11-03 22:52:21 +0530

    Add files via upload

---
+++ terraform/Modules/ec2/variables.tf
@@ -0,0 +1,23 @@
+variable "name" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "vpc_id" {
+  type = string
+}
+
+variable "env" {
+  type = string
+}
